46d3576accb90870f3b859dec82db650b3aa374c
kyantikov <yantikovkirill@gmail.com>
2019-12-10 02:30:19 -0600

    rename model to fit convention

--- Backend/database/models/securities.js
+++ Backend/database/models/securities.js
@@ -1,11 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Securities = sequelize.define('Securities', {
+  const securities = sequelize.define('securities', {
     ticker: DataTypes.STRING,
     name: DataTypes.STRING
   }, {});
-  Securities.associate = function(models) {
+  securities.associate = function(models) {
     // associations can be defined here
   };
-  return Securities;
+  return securities;
 };
