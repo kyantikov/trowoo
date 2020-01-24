016724147e83f707be15365ab9ce1d4a501de398
kyantikov <yantikovkirill@gmail.com>
2020-01-23 21:20:55 -0600

    remove obj/; update .gitignore

---
+++ backendSharp/Controllers/SecurityController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backendSharp.Controllers
+{
+    public class SecurityController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- backendSharp/trowoo_dev_db_sharpContext.cs
+++ backendSharp/trowoo_dev_db_sharpContext.cs
@@ -8,12 +8,12 @@ namespace backendSharp
     public partial class trowoo_dev_db_sharpContext : DbContext
     {
         public DbSet<Security> Security {get; set;}
-        public DbSet<Position> Position {get; set;}
         public DbSet<Portfolio> Portfolio {get;set;}
+        public DbSet<Position> Position {get; set;}
         public DbSet<Quote> Quote {get;set;}
-        public DbSet<TrailingStop> TrailingStop {get;set;}
         public DbSet<LowPrice> LowPrice {get;set;}
         public DbSet<HighPrice> HighPrice {get;set;}
+        public DbSet<TrailingStop> TrailingStop {get;set;}
         public trowoo_dev_db_sharpContext()
         {
         }
