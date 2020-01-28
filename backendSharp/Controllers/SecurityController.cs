591717e89b4a29012992dbddf5858af606723ff6
kyantikov <yantikovkirill@gmail.com>
2020-01-28 15:13:57 -0600

    document Security service and controller

--- backendSharp/Controllers/SecurityController.cs
+++ backendSharp/Controllers/SecurityController.cs
@@ -8,6 +8,14 @@ using Trowoo.Services;
 
 namespace Trowoo.Controllers
 {
+    /// <summary>
+    /// API controller class which inherits ControllerBase class, uses SecurityService process/recieve Security related data.
+    /// ROUTE: '/security'
+    /// All methods return an HTTP Response.
+    /// </summary>
+    /// <remarks>
+    /// <para>This class contains methods which are similarlly named in SecurityService.</para>
+    /// </remarks>
     [ApiController]
     [Route("security")]
     public class SecurityController : ControllerBase
@@ -15,18 +23,36 @@ namespace Trowoo.Controllers
         private SecurityService SecurityService;
         private readonly ILogger<SecurityController> Logger;
 
+        /// <summary>
+        /// Constructor method injects SecurityService and Logger into the class upon instantiation.
+        /// </summary>
+        /// <param name="securityService">SecurityService Dependency Injection.</param>
+        /// <param name="logger">Logger Dependency Injection.</param>
         public SecurityController(SecurityService securityService, ILogger<SecurityController> logger)
         {
             SecurityService = securityService;
             Logger = logger;
         }
+
+        /// <summary>
+        /// GET request to retrieve all Securities.
+        /// </summary>
+        /// <returns>List of all Securities in database.</returns>
         [HttpGet]
         public ActionResult<List<Security>> GetAll()
         {
             return SecurityService.GetAll();
         }
 
-        [HttpGet("{id}")]
+        /// <summary>
+        /// GET request to retrieve a Security by id.
+        /// </summary>
+        /// <param name="id">Security Id. An integer.</param>
+        /// <returns>
+        /// <para>Returns 200 if Security exists. Security object returned.</para>
+        /// <para>Returns 404 if Security does not exist.</para>
+        /// </returns>
+        [HttpGet("{id:int}")]
         public ActionResult<Security> GetById(int id)
         {
             var security = SecurityService.GetById(id);
@@ -37,7 +63,14 @@ namespace Trowoo.Controllers
             return Ok(security);
         }
         
-
+        /// <summary>
+        /// POST request to create Security.
+        /// </summary>
+        /// <param name="security">Security object.</param>
+        /// <returns>
+        /// <para>Returns 201 if Security is successfully validated, created, and saved.</para>
+        /// <para>Returns 404 if validation fails and Security is not saved. </para>
+        /// </returns>
         [HttpPost]
         public ActionResult<Security> FindOrCreate([FromBody] Security security)
         {
@@ -45,6 +78,19 @@ namespace Trowoo.Controllers
             return CreatedAtAction(nameof(GetById), new {id = security.Id}, security);
         }
 
+        /// <summary>
+        /// PUT request to update existing Security.
+        /// </summary>
+        /// <param name="security">Updated Security object.</param>
+        /// <returns>
+        /// <exception cref="Trowoo.Services.EntityExistsException">
+        /// Throws when attempting to update an existing Security's ticker to a ticker which already exists in the database.
+        /// </exception>
+        /// <para>Returns 200 if the Security was successfully validated and updated. Updated Security object returned.</para>
+        /// <para>Returns 404 if the Security with specified id does not exist.</para>
+        /// <para>Returns 404 if updating the Security to new values fails the model validations.</para>
+        /// <para>Returns 409 if updating Security.ticker results in an EntityExistsException.</para>
+        /// </returns>
         [HttpPut]
         public ActionResult<Security> Update([FromBody] Security security)
         {
@@ -64,7 +110,20 @@ namespace Trowoo.Controllers
             }
         }
 
-        [HttpDelete("{id}")]
+        /// <summary>
+        /// DELETE request to delete a Security.
+        /// </summary>
+        /// <param name="id">Security Id. An integer.</param>
+        /// <returns>
+        /// <para>
+        /// <exception cref="Trowoo.Services.EntityDoesNotExistException">
+        /// Throws when attempting to delete a Security that does not exist.
+        /// </exception>
+        /// </para>
+        /// <para>Returns 200 if the Security was successfully deleted.</para>
+        /// <para>Returns 404 if the Security with specified id does not exist.</para>
+        /// </returns>
+        [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
             try
--- backendSharp/Services/SecurityService.cs
+++ backendSharp/Services/SecurityService.cs
@@ -6,13 +6,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Trowoo.Services
 {
+    /// <summary>
+    /// Service layer class containing all methods for Security related database operations.
+    /// </summary>
+    /// <remarks>
+    /// <para>This class has the following methods:</para>
+    /// <para>FindOrCreate, GetAll, Update, Delete, GetById, GetByTicker</para>
+    /// </remarks>
     public class SecurityService
     {
         private TrowooDbContext TrowooDbContext;
+        /// <summary>
+        /// Constructor method injects the dependency of type TrowooDbContext into the class upon instantiation.
+        /// </summary>
+        /// <param name="trowooDbContext">Database Context dependecy injection.</param>
         public SecurityService(TrowooDbContext trowooDbContext)
         {
             TrowooDbContext = trowooDbContext;
         }
+
+        /// <summary>
+        /// Creates Security in DB if !exists.
+        /// </summary>
+        /// <param name="security">Security object.</param>
+        /// <returns>Security object originally passed as an argument.</returns>
         public Security FindOrCreate(Security security)
         {
             var existingSecurity = GetByTicker(security.Ticker);
@@ -25,10 +42,21 @@ namespace Trowoo.Services
             TrowooDbContext.SaveChanges();
             return security;
         }
+
+        /// <summary>
+        /// Retrieves all Securities in the database.
+        /// </summary>
+        /// <returns>List of all Securities.</returns>
         public List<Security> GetAll()
         {
             return TrowooDbContext.Securities.ToList();
         }
+
+        /// <summary>
+        /// Updates a Security for a specified id.
+        /// </summary>
+        /// <param name="security">Updated Security object.</param>
+        /// <returns>Updated Security object.</returns>
         public Security Update(Security security)
         {
             var existingSecurity = GetById(security.Id);
@@ -45,6 +73,10 @@ namespace Trowoo.Services
             return security;
         }
         
+        /// <summary>
+        /// Delete's Security with specified id.
+        /// </summary>
+        /// <param name="id">Security Id. An integer.</param>
         public void Delete(int id)
         {
             try
@@ -57,10 +89,22 @@ namespace Trowoo.Services
                 throw new EntityDoesNotExistException($"Security with id: '{id}' does not exist", exception);
             }
         }
+
+        /// <summary>
+        /// Retrieves Security with specified id.
+        /// </summary>
+        /// <param name="id">Security Id. An integer.</param>
+        /// <returns>Security object.</returns>
         public Security GetById(int id)
         {
             return TrowooDbContext.Securities.Find(id);
         }
+
+        /// <summary>
+        /// Retrieves Security with specified ticker.
+        /// </summary>
+        /// <param name="ticker">Security Ticker. A string.</param>
+        /// <returns>Security object.</returns>
         public Security GetByTicker(string ticker)
         {
             return TrowooDbContext.Securities.Where(s => s.Ticker == ticker).FirstOrDefault();
