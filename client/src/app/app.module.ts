6d257b67a8a43f77ec178baa46de737e64b2f0f6
kyantikov <yantikovkirill@gmail.com>
2020-04-04 17:53:24 -0500

    rename configs->config; add to .gitignore

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
@@ -17,6 +18,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CoreModule,
     SharedModule,
     HomeModule,
---
+++ client/src/app/config/okta.config.ts
@@ -0,0 +1,7 @@
+export default {
+  clientId: '0oa26b0fwuZgO5hOB357',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  issuer: 'https://dev-793026.okta.com/oauth2/default',
+  scopes: ['openid'],
+  pkce: true,
+};
