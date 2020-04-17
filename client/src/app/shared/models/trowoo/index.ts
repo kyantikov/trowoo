3008e2250259d3818e5d03978b4b56290787c11a
kyantikov <yantikovkirill@gmail.com>
2020-04-16 19:02:08 -0500

    export all trowoo-related classes through index.ts

---
+++ client/src/app/shared/models/trowoo/index.ts
@@ -0,0 +1,7 @@
+export * from './quote';
+export * from './security';
+export * from './position';
+export * from './portfolio';
+export * from './highPrice';
+export * from './lowPrice';
+export * from './trailingStop';
