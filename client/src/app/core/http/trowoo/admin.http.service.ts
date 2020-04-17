30ec1f91281da6ed5e6090a9fba9ddfba4e24964
kyantikov <yantikovkirill@gmail.com>
2020-04-16 19:09:39 -0500

    add method for system/quote route

--- client/src/app/core/http/trowoo/admin.http.service.ts
+++ client/src/app/core/http/trowoo/admin.http.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { AdminUrls } from '../../../shared/models/admin-urls.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminHttpService {
+  private baseUrl = 'https://localhost:5001/' ;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  retrieveQuotesFromAlphaVantage() {
+    return this.http.get(this.baseUrl + AdminUrls.quote);
+  }
 }
