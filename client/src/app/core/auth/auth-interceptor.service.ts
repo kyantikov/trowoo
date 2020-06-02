151f3df24a3fa6e258cd1063a22ebd824d0806b5
kyantikov <yantikovkirill@gmail.com>
2020-06-02 16:19:34 -0500

    implement AuthInterceptor logic based off getAccessToken in auth.service.ts

--- client/src/app/core/auth/auth-interceptor.service.ts
+++ client/src/app/core/auth/auth-interceptor.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
+import { AccessToken } from '@okta/okta-angular/src/okta/models/token-manager';
+
 import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+import {mergeMap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    console.log('AuthInterceptor created!');
+  }
 
-  // @ts-ignore
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+    return this.authService.getAccessToken()
+      .pipe(
+        mergeMap(token => {
+          req = req.clone({
+            setHeaders: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          return next.handle(req);
+        })
+      );
   }
 }
