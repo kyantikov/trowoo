42c6360aba20aa810d5d715ade39d849d72a3146
kyantikov <yantikovkirill@gmail.com>
2020-06-08 23:50:34 -0500

    implement data store for grid based on PortfolioCustomStore

--- client/src/app/core/services/portfolio.service.ts
+++ client/src/app/core/services/portfolio.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import CustomStore from 'devextreme/data/custom_store';
+
+import PortfolioCustomStore from './stores/portfolio.custom-store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
+  store: CustomStore;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.store = new PortfolioCustomStore(this.http);
+  }
 }
