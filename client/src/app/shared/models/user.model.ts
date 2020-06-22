41ad8db4347451c1bb66cb24553cb9d00d4d5cdc
kyantikov <yantikovkirill@gmail.com>
2020-06-22 17:38:26 -0500

    trim user model; remove expTimestamp field

--- client/src/app/shared/models/user.model.ts
+++ client/src/app/shared/models/user.model.ts
@@ -2,26 +2,16 @@ export class User {
   public fullName: string;
 
   constructor(
-    private id: string,
+    // tslint:disable-next-line:variable-name
+    private _id: string,
     public firstName: string,
     public lastName: string,
     public email: string,
-    private expTimestamp: number,
   ) {
     this.fullName = firstName + ' ' + lastName;
   }
 
-  get _id() {
-    if (!this.expTimestamp || new Date().getTime() > (this.expTimestamp * 1000)) {
-      return null;
-    }
-    return this.id;
-  }
-
-  get _tokenExp() {
-    if (!this.expTimestamp || new Date().getTime() > (this.expTimestamp * 1000)) {
-      return null;
-    }
-    return this.expTimestamp * 1000;
+  get id() {
+    return this._id;
   }
 }
