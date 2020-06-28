f9109fc053da8b7945d44abc23d6468e49e1f958
kyantikov <yantikovkirill@gmail.com>
2020-06-28 18:43:42 -0500

    setup basic popup for adding new positions (w/ TODO); disable updating of performanceMetrics fields

--- client/src/app/modules/all-positions-grid/all-positions-grid.module.ts
+++ client/src/app/modules/all-positions-grid/all-positions-grid.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 
+import { DxAutocompleteModule, DxDateBoxModule } from 'devextreme-angular';
+
 import { SharedModule } from '../../shared/shared.module';
 import { AllPositionsGridRoutingModule } from './all-positions-grid-routing.module';
 
@@ -7,10 +9,14 @@ import { AllPositionsGridComponent } from './pages/all-positions-grid.component'
 
 
 @NgModule({
-  declarations: [AllPositionsGridComponent],
+  declarations: [
+    AllPositionsGridComponent
+  ],
   imports: [
     SharedModule,
     AllPositionsGridRoutingModule,
+    DxAutocompleteModule,
+    DxDateBoxModule,
   ],
 })
 export class AllPositionsGridModule { }
